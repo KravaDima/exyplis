5c338a707cf07bf0d5e04d13050b1b8ba4e2c2b1
Dmitrij <KravaDima@gmail.com>
2018-02-08 10:05:29 +0200

    correct redirect

--- exyplis.sql
+++ exyplis.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.3
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Фев 08 2018 г., 00:53
--- Версия сервера: 5.6.37
--- Версия PHP: 5.6.31
+-- Время создания: Фев 08 2018 г., 10:59
+-- Версия сервера: 5.7.16
+-- Версия PHP: 5.6.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -33,6 +31,14 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1);
 
 -- --------------------------------------------------------
 
@@ -66,8 +72,9 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`id`, `user_id`, `name`, `text`, `deadline`, `status`) VALUES
-(1, 1, 'Тестовое задание', 'Простейшее приложение для работы с задачами, предпочтительно  с использованием фреймворков Laravel+VueJS (или любые другие)\r\n\r\nСоздание, обновление, удаление задач', '2018-02-08', 1),
-(2, 2, 'Тестовое задание (продолжение)', 'Каждая задача - строка с названием таска, дата необходимого выполения\r\n\r\nПриложения доступно только для аутентифицированных пользователей. Регистрация-аутентификация пойдет из коробки\r\n\r\nАвторизация доступа к таскам -  каждый пользователь может обновлять и удалять только свои таски', '2018-02-08', 1);
+(2, 1, 'Тестовое задание (продолжение)', 'Каждая задача - строка с названием таска, дата необходимого выполения\n\nПриложения доступно только для аутентифицированных пользователей. Регистрация-аутентификация пойдет из коробки\n\nАвторизация доступа к таскам -  каждый пользователь может обновлять и удалять только свои таски', '2018-02-07', 1),
+(3, 1, 'Тестовое задание', 'Простейшее приложение для работы с задачами, предпочтительно  с использованием фреймворков Laravel+VueJS (или любые другие)\n\nСоздание, обновление, удаление задач', '2018-02-07', 1),
+(10, 2, 'test', 'test', '2018-02-08', 0);
 
 -- --------------------------------------------------------
 
@@ -76,7 +83,7 @@ INSERT INTO `tasks` (`id`, `user_id`, `name`, `text`, `deadline`, `status`) VALU
 --
 
 CREATE TABLE `users` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -90,7 +97,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@gmail.com', '$2y$10$H.IEyQjT.g4Tueq.FEwv.OTmsckzrzScaP4UshcyOR9iQSHGYGpwS', NULL, '2018-02-07 19:46:52', '2018-02-07 19:46:52');
+(1, 'admin', 'admin@gmail.com', '$2y$10$yONe9sw9zvhvTfjzdIjsx.mnsu0lPHyigfOVSl8m5Pa0BxXqZPCge', 'b6m5zgFGZje6wcIVPnExWKiXmHwbY975z2hAA9dHWvU5ZSVVzcYmkgj4mamd', '2018-02-06 12:42:33', '2018-02-06 12:42:33'),
+(2, 'test', 'test@test.com', '$2y$10$o0hfNjSqyJkysyIl8QcdSedmL0IBZ1Jl.bXuvsnh06hvjFvGQ1gdi', NULL, '2018-02-08 05:57:19', '2018-02-08 05:57:19');
 
 --
 -- Индексы сохранённых таблиц
@@ -101,6 +109,12 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `creat
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`(191));
 
 --
 -- Индексы таблицы `tasks`
@@ -119,21 +133,15 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT для таблицы `migrations`
---
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT для таблицы `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
-
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
